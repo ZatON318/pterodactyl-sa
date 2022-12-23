5bcad7886291b9d7edf5cd2a5844d56f93909d76
daniscript18 <daniscript18@outlook.com>
2022-12-23 14:54:00 +0000

    Minor changes and improvements
    Add the "scripts" folder to store the box64 and box86 install scripts there, although I plan to use it to add more scripts.
    
    I added the variable "MAX_PLAYERS" in all the eggs, that will allow to adjust the number of maximum people that can enter a server (adjustable only by administrators).
    
    I removed a couple of unnecessary lines in the MTA install script (in the egg).
    
    And lastly I changed the URL where the docker image will download the "install-box64.sh" and "install-box86.sh" files.

---
+++ scripts/install-box86.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ARCH=$(uname -i)
+
+if [ "$ARCH" == "aarch64" ]; then
+    apt update -y ; apt upgrade -y ; apt install -y build-essential cmake make python3
+    dpkg --add-architecture armhf ; apt update -y ; apt upgrade -y ; apt install -y libc6:armhf gcc-arm-linux-gnueabihf
+    git clone https://github.com/ptitSeb/box86 && cd box86 ; mkdir build ; cd build ; cmake .. -DRPI4ARM64=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo ; make -j2 ; make install
+fi
