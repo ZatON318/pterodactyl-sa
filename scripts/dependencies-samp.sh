fb989cd05d0d6dd6190f574027a25372189b7d9d
daniscript18 <daniscript18@outlook.com>
2024-10-11 01:09:52 -0400

    Dependency issues fixed

--- scripts/dependencies-samp.sh
+++ scripts/dependencies-samp.sh
@@ -25,6 +25,15 @@ Signed-By: /usr/share/keyrings/box86-archive-keyring.gpg" | tee /etc/apt/sources
         else
             apt install box86-generic-arm -y
         fi
+    else
+        dpkg --add-architecture i386
+        apt update -y ; apt upgrade -y
+        apt install -y libstdc++6 lib32stdc++6 tar wget curl iproute2 openssl fontconfig dirmngr ca-certificates dnsutils tzdata zip
+        if [ "$(uname -m)" = "x86_64" ]; then \
+            wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_i386.deb && \
+            dpkg -i libssl1.1_1.1.0g-2ubuntu4_i386.deb && \
+            rm libssl1.1_1.1.0g-2ubuntu4_i386.deb; \
+        fi
     fi
 }
 
